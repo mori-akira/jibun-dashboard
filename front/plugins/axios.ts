ee4e13070daa86cce2a8881546116fc00380e543
mori-akira <77237845+mori-akira@users.noreply.github.com>
2025-07-03 09:17:31 +0900

    #9

--- front/plugins/axios.ts
+++ front/plugins/axios.ts
@@ -2,9 +2,9 @@ import { defineNuxtPlugin, useRuntimeConfig } from "#app";
 import axios from "axios";
 import urlJoin from "url-join";
 
-export default defineNuxtPlugin(() => {
-  const config = useRuntimeConfig();
+const config = useRuntimeConfig();
 
+export default defineNuxtPlugin(() => {
   axios.defaults.baseURL = config.public.apiBaseUrl as string;
 
   const instance = axios.create();
