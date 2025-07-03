8a67f74c86c9ea85867f38afa1c8e52d188b981d
mori-akira <77237845+mori-akira@users.noreply.github.com>
2025-07-03 09:05:26 +0900

    #9

--- front/nuxt.config.ts
+++ front/nuxt.config.ts
@@ -6,18 +6,21 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ["~/asset/css/main.css"],
   modules: ["@nuxt/content", "@nuxt/eslint", "@nuxt/ui", "@nuxt/icon"],
+  runtimeConfig: {
+    public: {
+      apiBaseUrl: process.env.NUXT_PUBLIC_API_BASE_URL || "",
+      apiMode: process.env.NUXT_PUBLIC_API_MODE || "default",
+    },
+  },
+  app: {
+    baseURL: process.env.NUXT_PUBLIC_BASE_URL || "/",
+  },
   vite: {
     plugins: [tailwindcss()],
     server: {
       watch: {
         usePolling: true,
       },
-    },
-  },
-  runtimeConfig: {
-    public: {
-      apiBaseUrl: process.env.NUXT_PUBLIC_API_BASE_URL || "",
-      apiMode: process.env.NUXT_PUBLIC_API_MODE || "default",
     },
   },
 });
