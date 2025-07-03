82da7b4bd6dc2b7f93d3c0a3d0483b7526a9a32b
mori-akira <77237845+mori-akira@users.noreply.github.com>
2025-07-03 09:20:22 +0900

    #9

--- front/plugins/axios.ts
+++ front/plugins/axios.ts
@@ -2,23 +2,19 @@ import { defineNuxtPlugin, useRuntimeConfig } from "#app";
 import axios from "axios";
 import urlJoin from "url-join";
 
-const config = useRuntimeConfig();
-
 export default defineNuxtPlugin(() => {
+  const config = useRuntimeConfig();
   axios.defaults.baseURL = config.public.apiBaseUrl as string;
 
   const instance = axios.create();
   if (config.public.apiMode === "mock") {
     instance.interceptors.request.use((req) => {
       const url = req.url?.replace(/^\/?/, "");
-      req.url = urlJoin(
-        config.public.baseUrl as string,
-        "mock-api",
-        `${url}.json`
-      );
+      req.url = urlJoin("mock-api", `${url}.json`);
       req.method = "get";
-      req.baseURL = "";
-      console.log("Mock API request:", req.url);
+      req.baseURL = config.public.baseUrl as string;
+      console.log("Mock BASE URL:", req.baseURL);
+      console.log("Mock API URL:", req.url);
       return req;
     });
   }
