e28d15ad7cfb11a6d5b50db54431b500a93c1f34
mori-akira <77237845+mori-akira@users.noreply.github.com>
2025-07-05 13:29:59 +0900

    Update index.vue

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -3,23 +3,18 @@
     <Icon name="tabler:home" class="adjust-icon" />
     <span class="font-cursive font-bold ml-2">Home</span>
     <div>
-      <button type="button" @click="onAddError">エラー追加</button>
-    </div>
-    <div>
-      <button type="button" @click="onClearError">エラークリア</button>
+      <Button wrapper-class="mt-4" @click="onAddError"> Add Error </Button>
     </div>
   </h2>
 </template>
 
 <script setup lang="ts">
 import { useCommonStore } from "~/stores/common";
+import Button from "~/components/common/Button.vue";
 const commonStore = useCommonStore();
 
 const onAddError = () => {
   commonStore.addErrorMessage("This is a test error message.");
-};
-const onClearError = () => {
-  commonStore.clearErrorMessages();
 };
 </script>
 
