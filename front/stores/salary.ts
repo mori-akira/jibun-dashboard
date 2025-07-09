7137a5c833aa73b80acb378b4ec1f5b3b72d679c
mori-akira <77237845+mori-akira@users.noreply.github.com>
2025-07-09 22:29:11 +0900

    #10

---
+++ front/stores/salary.ts
@@ -0,0 +1,36 @@
+import { defineStore } from "pinia";
+import { ref } from "vue";
+
+import type { Salary } from "~/api/client/api";
+import { SalaryApi } from "~/api/client/api";
+import { useCommonStore } from "~/stores/common";
+import { getErrorMessage } from "~/utils/error";
+
+export const useSalaryStore = defineStore("salary", () => {
+  const salaryApi = new SalaryApi();
+  const commonStore = useCommonStore();
+  const salaries = ref<Salary[] | null>(null);
+
+  async function fetchSalary(
+    targetDate?: string,
+    targetDateFrom?: string,
+    targetDateTo?: string
+  ) {
+    try {
+      const res = await salaryApi.getSalary(
+        targetDate,
+        targetDateFrom,
+        targetDateTo
+      );
+      salaries.value = res.data;
+    } catch (error) {
+      console.error("Failed to call api:", error);
+      commonStore.addErrorMessage(getErrorMessage(error));
+    }
+  }
+
+  return {
+    salaries,
+    fetchSalary,
+  };
+});
