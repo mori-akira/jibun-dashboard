4a9c9a99cc3ad5e1204798082a654c87a1984258
mori-akira <kgrmrak@yahoo.co.jp>
2025-07-26 14:06:44 +0000

    \#2

--- infra/main/modules/cognito/main.tf
+++ infra/main/modules/cognito/main.tf
@@ -9,11 +9,23 @@ resource "aws_cognito_user_pool_client" "user_pool_client" {
   user_pool_id                         = aws_cognito_user_pool.user_pool.id
   generate_secret                      = false
   allowed_oauth_flows_user_pool_client = true
+  allowed_oauth_flows                  = ["code", "implicit"]
+  allowed_oauth_scopes                 = ["openid"]
+
+  callback_urls = ["http://localhost:3000/callback"]
+  logout_urls   = ["http://localhost:3000/logout"]
+
+  supported_identity_providers = ["COGNITO"]
   explicit_auth_flows = [
     "ALLOW_USER_PASSWORD_AUTH",
     "ALLOW_REFRESH_TOKEN_AUTH",
     "ALLOW_USER_SRP_AUTH",
   ]
+}
+
+resource "aws_cognito_user_pool_domain" "domain" {
+  domain       = "jibun-dashboard"
+  user_pool_id = aws_cognito_user_pool.user_pool.id
 }
 
 output "user_pool_id" {
