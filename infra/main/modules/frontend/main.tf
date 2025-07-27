a32fa5ea285567c4c5ab93548eb300cf035ff2b4
mori-akira <kgrmrak@yahoo.co.jp>
2025-07-27 13:40:48 +0000

    \#2

--- infra/main/modules/frontend/main.tf
+++ infra/main/modules/frontend/main.tf
@@ -3,6 +3,23 @@ resource "aws_s3_bucket" "frontend" {
   tags   = var.application_tag
 }
 
+resource "aws_s3_bucket_policy" "frontend_policy" {
+  bucket = aws_s3_bucket.frontend.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = ["s3:GetObject"]
+        Resource  = ["${aws_s3_bucket.frontend.arn}/*"]
+      }
+    ]
+  })
+}
+
 resource "aws_s3_bucket_website_configuration" "frontend_website" {
   bucket = aws_s3_bucket.frontend.id
 
