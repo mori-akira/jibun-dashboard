226ce02df4cf338d03a25c06d56e0bee2f043ae5
mori-akira <77237845+mori-akira@users.noreply.github.com>
2025-08-21 00:06:21 +0900

    #27

--- back/services/resource/src/main/kotlin/com/github/moriakira/jibundashboard/resource/StreamLambdaHandler.kt
+++ back/services/resource/src/main/kotlin/com/github/moriakira/jibundashboard/resource/StreamLambdaHandler.kt
@@ -11,6 +11,7 @@ import java.io.OutputStream
 
 class StreamLambdaHandler : RequestStreamHandler {
 
+    @Suppress("TooGenericExceptionCaught")
     companion object {
         val handler: SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> = try {
             SpringBootLambdaContainerHandler.getAwsProxyHandler(LambdaApplication::class.java)
