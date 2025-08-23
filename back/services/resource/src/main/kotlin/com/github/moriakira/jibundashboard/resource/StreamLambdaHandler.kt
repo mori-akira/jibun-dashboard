92b31ebce63ae5e01ade7b6360d31fdc43f343f2
mori-akira <77237845+mori-akira@users.noreply.github.com>
2025-08-24 01:10:48 +0900

    #27

--- back/services/resource/src/main/kotlin/com/github/moriakira/jibundashboard/resource/StreamLambdaHandler.kt
+++ back/services/resource/src/main/kotlin/com/github/moriakira/jibundashboard/resource/StreamLambdaHandler.kt
@@ -1,8 +1,7 @@
 package com.github.moriakira.jibundashboard.resource
 
 import com.amazonaws.serverless.proxy.internal.LambdaContainerHandler
-import com.amazonaws.serverless.proxy.model.HttpApiV2ProxyRequest
-import com.amazonaws.serverless.proxy.spring.SpringBootProxyHandlerBuilder
+import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler
 import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler
 import java.io.InputStream
@@ -16,10 +15,10 @@ class StreamLambdaHandler : RequestStreamHandler {
             LambdaContainerHandler.getContainerConfig().initializationTimeout = 60_000
         }
 
-        private val handler = SpringBootProxyHandlerBuilder<HttpApiV2ProxyRequest>()
-            .defaultHttpApiV2Proxy()
-            .springBootApplication(LambdaApplication::class.java)
-            .buildAndInitialize()
+        private val handler =
+            SpringBootLambdaContainerHandler.getHttpApiV2ProxyHandler(
+                LambdaApplication::class.java,
+            )
     }
 
     override fun handleRequest(input: InputStream, output: OutputStream, context: Context) {
