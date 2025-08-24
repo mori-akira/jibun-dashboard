b60087bdcecefb0463226d9f5bc98e138240312f
mori-akira <77237845+mori-akira@users.noreply.github.com>
2025-08-24 15:29:36 +0000

    \#15

--- front/middleware/auth.global.ts
+++ front/middleware/auth.global.ts
@@ -20,7 +20,7 @@ export default defineNuxtRouteMiddleware((to) => {
     const region = config.public.region;
     const domain = config.public.cognitoDomain;
     const redirectUri = window.location.origin + "/callback";
-    const loginUrl = `https://${domain}.auth.${region}.amazoncognito.com/login?response_type=token+id_token&client_id=${clientId}&redirect_uri=${redirectUri}`;
+    const loginUrl = `https://${domain}.auth.${region}.amazoncognito.com/login?response_type=token&client_id=${clientId}&redirect_uri=${redirectUri}`;
     console.log("Redirecting to login:", loginUrl);
     window.location.href = loginUrl;
   }
