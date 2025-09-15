c118211b2bdca518995ed6be0622bc6dbff8e9b2
mori-akira <77237845+mori-akira@users.noreply.github.com>
2025-09-15 18:34:26 +0900

    #44

--- back/src/main/kotlin/com/github/moriakira/jibundashboard/service/CognitoUserService.kt
+++ back/src/main/kotlin/com/github/moriakira/jibundashboard/service/CognitoUserService.kt
@@ -7,14 +7,13 @@ import org.springframework.web.reactive.function.client.WebClient
 import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.cognitoidentityprovider.CognitoIdentityProviderClient
 import software.amazon.awssdk.services.cognitoidentityprovider.model.ChangePasswordRequest
-import software.amazon.awssdk.services.cognitoidentityprovider.model.CognitoIdentityProviderException
 import java.time.Duration
 
 @Service
 class CognitoUserService(
     private val webClient: WebClient,
     @param:Value("\${app.security.cognito.base-uri:}") private val baseUri: String?,
-    @param:Value("\${app.security.cognito.region:ap-northeast-1}") private val cognitoRegion: String
+    @param:Value("\${app.security.cognito.region:ap-northeast-1}") private val cognitoRegion: String,
 ) {
     fun fetch(accessToken: String): CognitoUserInfo = webClient.get()
         .uri("$baseUri/oauth2/userInfo")
@@ -34,7 +33,7 @@ class CognitoUserService(
                         .accessToken(accessToken)
                         .previousPassword(oldPassword)
                         .proposedPassword(newPassword)
-                        .build()
+                        .build(),
                 )
             }
     }
