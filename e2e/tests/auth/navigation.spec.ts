9a6500225e283354bcf49f2ab044a84ab4a13c9f
mori-akira <kgrmrak@yahoo.co.jp>
2025-10-04 01:50:15 +0000

    \#45

--- e2e/tests/auth/navigation.spec.ts
+++ e2e/tests/auth/navigation.spec.ts
@@ -1,21 +1,61 @@
 import { test, expect } from "@playwright/test";
 
 test("check navigation", async ({ page }) => {
-  // access to top
+  // top
   await page.goto("/", { waitUntil: "domcontentloaded" });
   await page.waitForLoadState("networkidle");
 
-  // access to salary
+  // salary
   await Promise.all([
     page.waitForURL("**/salary"),
     page.getByRole("link", { name: "Salary" }).click(),
   ]);
-  await expect(page.getByRole("main").getByText("Salary")).toBeVisible();
+  await expect(
+    page.getByRole("main").getByText("Salary", { exact: true })
+  ).toBeVisible();
 
-  // access to qualification
+  // qualification
   await Promise.all([
     page.waitForURL("**/qualification"),
     page.getByRole("link", { name: "Qualification" }).click(),
   ]);
-  await expect(page.getByRole("main").getByText("Qualification")).toBeVisible();
+  await expect(
+    page.getByRole("main").getByText("Qualification", { exact: true })
+  ).toBeVisible();
+
+  // vocabulary
+  await Promise.all([
+    page.waitForURL("**/vocabulary"),
+    page.getByRole("link", { name: "Vocabulary" }).click(),
+  ]);
+  await expect(
+    page.getByRole("main").getByText("Vocabulary", { exact: true })
+  ).toBeVisible();
+
+  // financial asset
+  await Promise.all([
+    page.waitForURL("**/financial-asset"),
+    page.getByRole("link", { name: "Financial Asset" }).click(),
+  ]);
+  await expect(
+    page.getByRole("main").getByText("Financial Asset", { exact: true })
+  ).toBeVisible();
+
+  // study plan
+  await Promise.all([
+    page.waitForURL("**/study-plan"),
+    page.getByRole("link", { name: "Study Plan" }).click(),
+  ]);
+  await expect(
+    page.getByRole("main").getByText("Study Plan", { exact: true })
+  ).toBeVisible();
+
+  // home
+  await Promise.all([
+    page.waitForURL("**/"),
+    page.getByRole("link", { name: "Home" }).click(),
+  ]);
+  await expect(
+    page.getByRole("main").getByText("Home", { exact: true })
+  ).toBeVisible();
 });
