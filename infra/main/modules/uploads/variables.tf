a0d7d515895ac16e3fd206f3f3e0fbcf73480904
mori-akira <kgrmrak@yahoo.co.jp>
2025-11-09 04:25:55 +0000

    \#52

--- infra/main/modules/uploads/variables.tf
+++ infra/main/modules/uploads/variables.tf
@@ -28,9 +28,3 @@ variable "enable_force_tls_policy" {
   type        = bool
   default     = true
 }
-
-variable "enable_lifecycle" {
-  description = "S3ライフサイクルフラグ"
-  type        = bool
-  default     = true
-}
