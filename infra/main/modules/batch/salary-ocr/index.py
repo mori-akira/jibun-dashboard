66aa08fd26dee3d8fefeca968992b0dea7a7e260
mori-akira <kgrmrak@yahoo.co.jp>
2025-11-21 13:11:12 +0000

    \#50

--- infra/main/modules/batch/salary-ocr/index.py
+++ infra/main/modules/batch/salary-ocr/index.py
@@ -2,7 +2,7 @@ import json
 from typing import Any, Dict
 
 
-def main(event: Dict[str, Any], _: Any) -> Dict[str, Any]:
+def lambda_handler(event: Dict[str, Any], _: Any) -> Dict[str, Any]:
     print("Mock Salary OCR Lambda invoked.")
     print("Event:", json.dumps(event))
     return {
