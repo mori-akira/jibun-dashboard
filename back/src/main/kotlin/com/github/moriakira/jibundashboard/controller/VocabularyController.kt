68aff65deb6c4533d94012efdd30646e5bc2a35e
mori-akira <77237845+mori-akira@users.noreply.github.com>
2026-05-02 13:21:28 +0900

    #71

--- back/src/main/kotlin/com/github/moriakira/jibundashboard/controller/VocabularyController.kt
+++ back/src/main/kotlin/com/github/moriakira/jibundashboard/controller/VocabularyController.kt
@@ -26,6 +26,7 @@ import com.github.moriakira.jibundashboard.service.VocabularyTagService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDate
 import java.time.OffsetDateTime
 import java.util.UUID
 
